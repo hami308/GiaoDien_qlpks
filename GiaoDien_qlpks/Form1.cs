9e09ea420c89560f10d49da8b0f1df9771fb680b
LENOVO <LENOVO@LAPTOP-JKMABAVK>
2023-09-21 23:17:00 +0700

    Add project files.

---
+++ GiaoDien_qlpks/Form1.cs
@@ -0,0 +1,30 @@
+namespace GiaoDien_qlpks
+{
+    public partial class FormDangnhap : System.Windows.Forms.Form
+    {
+        public FormDangnhap()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
